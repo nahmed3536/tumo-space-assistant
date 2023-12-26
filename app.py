34996d05bf2a032e7900a0aa6247422949064e6b
Ahmed <nahmed3536@gmail.com>
2023-12-26 17:17:32 +0400

    prototype planets graphics

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+# Function to display planet information
+def display_planet_info(planet):
+    st.write(f"## {planet.capitalize()} Information")
+    # Add information about the planet
+    # You can customize this based on your needs
+
+def main():
+    st.title("Solar System Explorer")
+
+    planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"]
+
+    selected_planet = st.sidebar.selectbox("Select a Planet", planets)
+
+    # Display planet icon in the sidebar
+    st.sidebar.image(f"images/{selected_planet}.png", use_column_width=True)
+
+    # Display planet information
+    display_planet_info(selected_planet)
+
+if __name__ == "__main__":
+    main()
